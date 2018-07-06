77c29bef385ee66a1cc5a41df10634543bc6a4d0
Samer Zagzoog <mrsamer.dev@gmail.com>
2018-07-06 16:18:13 +0300

    Added Components and root

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 
 // Bootstrap Imports
@@ -8,15 +9,28 @@ import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
 import { ModalModule } from 'ngx-bootstrap/modal';
 
+// Component Imports
+import { MainComponent } from './views/main/main.component';
+import { NavbarComponent } from './views/navbar/navbar.component';
+
+
+const appRoutes: Routes = [
+  {path: '', component: MainComponent}
+];
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     BsDropdownModule.forRoot(),
     TooltipModule.forRoot(),
-    ModalModule.forRoot()
+    ModalModule.forRoot(),
+    FormsModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
